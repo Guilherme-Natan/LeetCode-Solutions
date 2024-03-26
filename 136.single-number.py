1d6c2081df100d2c6c1cba86766b018ccd918dfe
Guilherme Natan <guilherme.natan.braga@gmail.com>
2024-03-26 18:00:18 -0300

    My (bad) solution to problem 136: Single Number

---
+++ 136.single-number.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=136 lang=python3
+#
+# [136] Single Number
+#
+
+
+# @lc code=start
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        first_set = set(nums)
+        final_set = set(nums)
+        for n in nums:
+            if n in first_set:
+                first_set.remove(n)
+            else:
+                final_set.remove(n)
+
+        return final_set.pop()
+
+
+# @lc code=end
