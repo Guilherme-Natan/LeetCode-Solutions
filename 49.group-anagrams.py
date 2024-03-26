fc93e7a09728c095f56c6afce1d2d316d2c70817
Guilherme Natan <guilherme.natan.braga@gmail.com>
2024-03-26 18:00:02 -0300

    My solution to problem 49: Group Anagrams

---
+++ 49.group-anagrams.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=49 lang=python3
+#
+# [49] Group Anagrams
+#
+
+# @lc code=start
+from collections import defaultdict
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        count_map = defaultdict(list)
+        for string in strs:
+            count_map[tuple(sorted(string))].append(string)
+        return count_map.values()
+
+
+# @lc code=end
