e186ecef553bcbda254fa17aa3d488b2f6c49478
Guilherme Natan <guilherme.natan.braga@gmail.com>
2024-03-30 17:34:49 -0300

    My (bad) solution to problem 42: Trapping rain water

--- 42.trapping-rain-water.py
+++ 42.trapping-rain-water.py
@@ -8,32 +8,36 @@
 # @lc code=start
 class Solution:
     def trap(self, height: list[int]) -> int:
-        # forward_pt = backward_pt = 0
-        forward_pt = 1
-        backward_pt = 0
+        forward_pt, backward_pt = 1, 0
         total_water = 0
         while backward_pt < len(height) - 1:
+            max_height_index, max_height = 0, 0
             while forward_pt < len(height):
-                if height[forward_pt] >= height[backward_pt]:
+                if height[forward_pt] > height[backward_pt]:
                     first_height = height[backward_pt]
-                    if (
-                        backward_pt > 0
-                        and height[backward_pt - 1] >= height[forward_pt]
-                    ):
-                        total_water += (forward_pt - backward_pt) * (
-                            height[forward_pt] - first_height
-                        )
                     backward_pt += 1
                     while backward_pt < forward_pt:
                         total_water += first_height - height[backward_pt]
                         backward_pt += 1
+                    break
+                if height[forward_pt] >= max_height:
+                    max_height = height[forward_pt]
+                    max_height_index = forward_pt
                 forward_pt += 1
-            backward_pt += 1
+            else:
+                forward_pt = max_height_index
+                backward_pt += 1
+                while backward_pt < forward_pt:
+                    total_water += max_height - height[backward_pt]
+                    backward_pt += 1
             forward_pt = backward_pt + 1
         return total_water
 
 
 # @lc code=end
+print(
+    f"Output: {Solution().trap([6, 5, 4, 3, 0, 0, 0, 7, 3, 2, 1, 4])}, Correct output: 30"
+)
 print(f"Output: {Solution().trap([0, 7, 1, 4, 6])}, Correct output: 7")
 print(f"Output: {Solution().trap([4, 2, 0, 3, 2, 4, 3, 4])}, Correct output: 10")
 print(
